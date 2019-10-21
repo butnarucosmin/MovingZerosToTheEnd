caee3a16dcc52299f378837d6829558471c16862
Cosmin Butnaru <cosminbutnaru18@gmail.com>
2019-10-21 15:01:01 +0300

    Add project files.

---
+++ MovingZerosToTheEnd/Kata.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovingZerosToTheEnd
+{
+    public class Kata
+    {
+        public static int[] MoveZeroes(int[] arr)
+        {
+            int[] result = arr;
+            int count = 0;
+
+            foreach(int value in arr)
+            {
+                if (value == 0)
+                {
+                    count++;
+                    result = result.Where(val => val != 0).ToArray();
+                }
+            }
+            for (int i = 0; i < count; i++)
+            {
+                result = result.Concat(new int[] { 0 }).ToArray();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ UnitTest.MovingZerosToTheEnd/Sample_Test.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MovingZerosToTheEnd;
+
+namespace UnitTest.MovingZerosToTheEnd
+{
+    [TestClass]
+    public class Sample_Test
+    {
+        [TestMethod]
+        public void Test()
+        {
+            CollectionAssert.AreEqual(new int[] { 1, 2, 1, 1, 3, 1, 0, 0, 0, 0 }, Kata.MoveZeroes(new int[] { 1, 2, 0, 1, 0, 1, 0, 3, 0, 1 }));
+        }
+    }
+}
